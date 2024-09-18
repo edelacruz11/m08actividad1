3883dc8352e70495975182f0176c4afa08525a6e
edelacruz <edelacruz@elpuig.xeill.net>
2024-09-18 17:42:40 +0200

    commit inicial

---
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.contador;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.contador.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int contador;
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        binding.augmentarElContador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador++;
+                binding.contadorDeClics.setText("Has clicado " + contador + " veces");
+            }
+        });
+    }
+}
